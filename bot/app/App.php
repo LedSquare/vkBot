cc55fba0ea6c1e32998c11173d0728cf9d205f51
leybkovich_a <leybkovich_a@lave.tech>
2024-10-09 16:22:16 +0300

    Fea: added vk api file, composer lock init. Fix: docker-compose and Dockerfile

---
+++ bot/app/App.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+final class App
+{
+    public static function run(): void
+    {
+
+    }
+}
--- bot/public/index.php
+++ bot/public/index.php
@@ -1,5 +1,13 @@
 <?php
+
 declare(strict_types=1);
 
+require_once '../vendor/autoload.php';
+require_once '../app/vk_api.php';
+
 use App\App;
+
+
+App::run();
+
 
